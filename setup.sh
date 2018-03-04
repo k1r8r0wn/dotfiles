4754186898dd8246668ea685cc7d57b771bcc92f
Kirill Bobrov <k1r8r0wn@gmail.com>
2018-03-04 18:34:16 +0600

    Add `setup` bash script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get current dir (so run this script from anywhere)
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo -e "Creating symlinks..."
+
+# bash_profile
+ln -s ${DOTFILES_DIR}/.bash_profile ~
+
+# zsh
+ln -s ${DOTFILES_DIR}/.zshrc ~
+ln -s ${DOTFILES_DIR}/.oh-my-zsh/themes/k1r8r0wn.zsh-theme ~/.oh-my-zsh/themes
+
+# vim
+ln -s ${DOTFILES_DIR}/.vimrc ~
+ln -s ${DOTFILES_DIR}/.vim/ ~
+
+echo -e "Done."
